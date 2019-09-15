fd8b381503955e7c19aaff0d9a1526c6362a8d47
Pe-Chih Chao <pccisme@gmail.com>
2019-09-15 00:24:06 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -101,7 +101,9 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
+      // console.log(categories[short_name]);
       var chosenCategoryShortName = chooseRandomCategory(categories);
+
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -114,15 +116,16 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", 
-        "'" + chosenCategoryShortName.short_name + "'");
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
+        "randomCategoryShortName", "'" + chosenCategoryShortName.short_name + "'");
+      // var homeHtmlToInsertIntoMainPage = ....
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-      insertHTML("#main-content", homeHtmlToInsertIntoMainPage);
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
@@ -318,12 +321,7 @@ function insertItemPrice(html,
   }
 
   priceValue = "$" + priceValue.toFixed(2);
-  html = insertPropecss
-fonts
-images
-js
-snippets
-index.htmlrty(html, pricePropName, priceValue);
+  html = insertProperty(html, pricePropName, priceValue);
   return html;
 }
 
